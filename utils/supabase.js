5afbef2fd19c23b972f637fc6611fdc4f6bc4bfa
kinzinzombe07 <kinzinzombe07@gmail.com>
2024-08-06 22:12:03 +0000

    [ADD]: Anon public key

--- utils/supabase.js
+++ utils/supabase.js
@@ -1,3 +1,5 @@
+import {config} from "dotenv";
+config()
 import { createClient } from '@supabase/supabase-js';
 
 const supabase = createClient(process.env.SUPABASE_URL, process.env.ANON_PUBLIC_KEY);
